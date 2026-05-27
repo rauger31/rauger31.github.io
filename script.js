a13e08ef05d3fffb727471c46d83fb39a9eacba7
rauger31 <8j7dch9rvf@privaterelay.appleid.com>
2026-05-27 23:43:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 const noButton = document.getElementById("noButton");
 const yesButton = document.getElementById("yesButton");
-
 const yesImage = document.querySelector(".yes-img");
-
 let attempts = 0;
 
-function moveButton(){
-
-    const maxX = window.innerWidth - noButton.offsetWidth - 20;
-    const maxY = window.innerHeight - noButton.offsetHeight - 20;
+function moveNo() {
+    const parent = noButton.parentElement; // .choice
+    const maxX = parent.offsetWidth - noButton.offsetWidth;
+    const maxY = parent.offsetHeight - noButton.offsetHeight;
 
     const x = Math.random() * maxX;
     const y = Math.random() * maxY;
@@ -16,44 +14,26 @@ function moveButton(){
     noButton.style.left = x + "px";
     noButton.style.top = y + "px";
 
-    // rotation douce 😄
     const rotation = Math.random() * 20 - 10;
     noButton.style.transform = `rotate(${rotation}deg)`;
 }
 
-function escapeButton(){
-
+function escapeButton() {
     attempts++;
 
-    // ❤️ bouton OUI grossit
+    // bouton OUI grossit
     const scale = 1 + attempts * 0.12;
-
     yesButton.style.transform = `scale(${scale})`;
-
-    // ❤️ image OUI grossit aussi
     yesImage.style.transform = `scale(${scale})`;
+    yesButton.style.boxShadow = `0 0 ${10 + attempts*5}px rgba(233,30,99,0.5)`;
 
-    yesButton.style.boxShadow =
-        `0 0 ${10 + attempts * 5}px rgba(233,30,99,0.5)`;
+    // texte du NON
+    if(attempts === 1) noButton.textContent = "Tu es sûr ? 😳";
+    if(attempts === 2) noButton.textContent = "Réfléchis 😭";
+    if(attempts >= 3) noButton.textContent = "Impossible 😈";
 
-    // 😈 texte du NON
-    if(attempts === 1){
-        noButton.textContent = "Tu es sûr ? 😳";
-    }
-
-    if(attempts === 2){
-        noButton.textContent = "Réfléchis 😭";
-    }
-
-    if(attempts >= 3){
-        noButton.textContent = "Impossible 😈";
-    }
-
-    moveButton();
+    moveNo();
 }
 
-// PC
 noButton.addEventListener("mouseenter", escapeButton);
-
-// Mobile
 noButton.addEventListener("touchstart", escapeButton);
