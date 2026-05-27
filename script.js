841236aa816ac88a3ce9c0c6ea3364c2551d5b26
rauger31 <8j7dch9rvf@privaterelay.appleid.com>
2026-05-28 00:06:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const noButton = document.getElementById("noButton");
 const yesButton = document.getElementById("yesButton");
-const yesImage = document.querySelector(".yes-img");
+
 let attempts = 0;
 
-function moveNo() {
-    const parent = noButton.parentElement; // .choice
-    const maxX = parent.offsetWidth - noButton.offsetWidth;
-    const maxY = parent.offsetHeight - noButton.offsetHeight;
+function moveButton(){
+
+    const maxX = window.innerWidth - noButton.offsetWidth;
+    const maxY = window.innerHeight - noButton.offsetHeight;
 
     const x = Math.random() * maxX;
     const y = Math.random() * maxY;
@@ -14,30 +14,37 @@ function moveNo() {
     noButton.style.left = x + "px";
     noButton.style.top = y + "px";
 
-    const rotation = Math.random() * 20 - 10;
+    // rotation fun 😄
+    const rotation = Math.random() * 40 - 20;
     noButton.style.transform = `rotate(${rotation}deg)`;
 }
 
-function escapeButton() {
+function escapeButton(){
+
     attempts++;
 
-    // bouton OUI grossit
-    const scale = 1 + attempts * 0.12;
+    // OUI grossit ❤️
+    const scale = 1 + attempts * 0.15;
     yesButton.style.transform = `scale(${scale})`;
-    yesImage.style.transform = `scale(${scale})`;
-    yesButton.style.boxShadow = `0 0 ${10 + attempts*5}px rgba(233,30,99,0.5)`;
 
-    // 🖤 NOIR & BLANC progressif
-    const grayscale = Math.min(attempts * 10, 100);
-    noImage.style.filter = `grayscale(${grayscale}%)`;
-    
-    // texte du NON
-    if(attempts === 1) noButton.textContent = "Tu es sûr ? 😳";
-    if(attempts === 2) noButton.textContent = "Réfléchis 😭";
-    if(attempts >= 3) noButton.textContent = "Impossible 😈";
+    // texte drôle 😈
+    if(attempts === 1){
+        noButton.textContent = "Tu es sûr ? 😳";
+    }
 
-    moveNo();
+    if(attempts === 2){
+        noButton.textContent = "Réfléchis 😭";
+    }
+
+    if(attempts >= 3){
+        noButton.textContent = "Impossible 😈";
+    }
+
+    moveButton();
 }
 
+// PC
 noButton.addEventListener("mouseenter", escapeButton);
+
+// Mobile
 noButton.addEventListener("touchstart", escapeButton);
